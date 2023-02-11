274752248b4f0bcc6dbbf6f630348446fb933354
foontinz <foontinz@protonmail.com>
2023-02-12 00:06:16 +0100

    docker-compose.yml added with postgres and backend app

--- backend/main.py
+++ backend/main.py
@@ -6,5 +6,7 @@ app = FastAPI()
 
 @app.get("/{query}")
 async def root(query):
-    return {"name": os.environ.get(""),
-            }
+    return {"name": os.environ.get("POSTGRES_DB"),
+            "user": os.environ.get("POSTGRES_USER"),
+            "password": os.environ.get("POSTGRES_PASSWORD"),
+            "nigger":os.environ.get("nigger")}
