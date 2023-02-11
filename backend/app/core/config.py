1896dae5354d701d3e7a8b51f1c705228788035a
foontinz <foontinz@protonmail.com>
2023-02-11 21:24:25 +0100

    layout progress

---
+++ backend/app/core/config.py
@@ -0,0 +1,25 @@
+from pydantic import PostgresDsn, BaseSettings, validator 
+from typing import Any
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    SQLALCHEMY_DATABASE_URI: PostgresDsn | None = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: str | None, values: dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+    JWT_ALGORITHM: str = "HS256"
