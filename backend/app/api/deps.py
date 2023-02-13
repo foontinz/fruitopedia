f6cba6371784cd99364e8bd3fb2ce58c28757ae7
foontinz <foontinz@protonmail.com>
2023-02-14 00:42:20 +0100

    added to /login/register/ user existence examination

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,6 +1,16 @@
 from typing import Generator
-from app.db.session import SessionLocal
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends
+from sqlalchemy.orm import Session
 
+from app.db.session import SessionLocal
+from app.core.config import settings
+from app import models
+from app import crud
+
+reusable_oauth2 = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_V1_STR}/login/access-token"
+)
 
 def get_db() -> Generator:
     try:
@@ -9,3 +19,7 @@ def get_db() -> Generator:
     finally:
         db.close()
 
+def current_user(
+    db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)
+) -> models.User:
+    return crud.user.get_by_email(db, email=token)
