70c2c81b50c035d52f4f1f9c436595b69470537a
foontinz <foontinz@protonmail.com>
2023-02-14 00:40:31 +0100

    changes create_access_token func to receive param models.User

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,19 +1,18 @@
 from app.core.config import settings
 from app.schemas.token import TokenPayload
+from app.models.user import User
 
 from bcrypt import hashpw
 from datetime import datetime, timedelta
 
 import jwt
 
-def create_access_token(
-        subject: str, 
-        admin: bool = False) -> str:
+def create_access_token(user:User) -> str:
     expire =  datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode = TokenPayload(
         exp=expire,
-        sub=subject, 
-        admin=admin).dict()
+        sub=user.id, 
+        admin=user.is_superuser).dict()
     
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
     return encoded_jwt
