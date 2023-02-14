a657c324d49b6a5d5f19f92954a52dca994a8121
dmitriy <dima22022005@gmail.com>
2023-02-14 22:16:12 +0100

    import react-globe.gl

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Globe from './components/Globe';
 import Home from './components/Home';
 
 const App = () => {
   return (
     <div>
       <Home></Home>
+      <Globe></Globe>
     </div>
   );
 };
---
+++ frontend/src/components/Globe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {default as Earth} from 'react-globe.gl'
+
+const Globe = () => {
+    return (
+        <div>
+            <Earth></Earth>
+        </div>
+    );
+};
+
+export default Globe;
