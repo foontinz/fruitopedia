9a5b87ab525ae66ea70884c0e1fd3adc21cb8505
foontinz <foontinz@protonmail.com>
2023-02-14 03:47:36 +0100

    changes auth in /login/access-token to auth with form_data instead of UserCredentials typecasting, changes /login/register to query for a user with crud.user.read_by_identifier

--- backend/app/api/api_v1/endpoints/login.py
+++ backend/app/api/api_v1/endpoints/login.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import Session
 
 from app import crud, schemas
 from app.utils.authorization import salt_hash_user_password
-from app.schemas.user import UserCredentials
 from app.api.deps import get_db
 from app.core.config import settings
 from app.core.security import create_access_token
@@ -21,7 +20,7 @@ async def access_token(
     db: Session = Depends(get_db), 
     form_data: OAuth2PasswordRequestForm = Depends()):
     
-    user = crud.user.authenticate(db, UserCredentials(email=form_data.username, password=form_data.password))
+    user = crud.user.authenticate(db, form_data)
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect email or password")
     access_token = create_access_token(subject=user.email, admin=user.is_superuser)
@@ -33,12 +32,12 @@ async def access_token(
 @router.post("/register", response_model=schemas.Token)
 async def register(
     db: Session = Depends(get_db), 
-    user: schemas.UserCreate = Body(...)):
+    user: schemas.UserCreateCredentials = Body(...)):
     
-    if crud.user.read(db, schemas.UserCreate(email=user.email, username=user.username)):
+    if crud.user.read_by_identifier(db, obj_in=schemas.UserRead(**user.dict(exclude_none=True))):
         raise HTTPException(status_code=400, detail="Email or Username already registered")
     
     user = salt_hash_user_password(user)
-    user = crud.user.create(db, obj_in=user)
+    user = crud.user.create(db, obj_in=schemas.UserCreate(**user.dict(exclude_none=True)))
     access_token = create_access_token(user)
     return schemas.Token(access_token=access_token, token_type="bearer")
