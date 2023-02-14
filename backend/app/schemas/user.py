c37566433b7c4093273a76a56516c2766d8d59ff
foontinz <foontinz@protonmail.com>
2023-02-14 15:03:39 +0100

    adds new schema UserLoginCredentials for login

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -28,6 +28,8 @@ class UserCreate(UserBase):
 
 
 class UserCreateCredentials(UserCreate):
+    email: EmailStr
+    username: str
     password: str
 
     @validator('password')
@@ -35,6 +37,12 @@ class UserCreateCredentials(UserCreate):
         if not re.search(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,}$', password):
             raise ValueError("Password must have at least 8 characters, 1 uppercase, 1 lowercase, 1 number and 1 special character")
         return password
+
+class UserLoginCredentials(UserBase):
+    identifier: str
+    password: str
+
+
 
 class UserDelete(UserBase):
     id: int
