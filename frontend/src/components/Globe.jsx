9ed72d8e676899534fd65135fbcfaf6ebf9bb909
dmitriy <dima22022005@gmail.com>
2023-02-15 16:37:44 +0100

    Gets rid of map texture. Makes earth material and generates random green shade to simulate earth-like terrain.

--- frontend/src/components/Globe.jsx
+++ frontend/src/components/Globe.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as THREE from 'three'
 import { useState, useEffect } from 'react';
 import { default as Earth } from 'react-globe.gl'
 
@@ -6,9 +7,14 @@ const Globe = () => {
     const [countries, setCountries] = useState([]);
 
 
+    // useEffect(() => {
+    //     // load specific country
+    //     fetch('countries.geojson').then(res => res.json()).then(res => setCountries(res.features.filter(f => f.properties.ISO_A3 == "AFG")));
+    // }, []);
+
     useEffect(() => {
-        // load data
-        fetch('countries.geojson').then(res => res.json()).then(res => setCountries(res.features.filter(f => f.properties.ISO_A3 == "AFG")));
+        // load each country
+        fetch('countries.geojson').then(res => res.json()).then(res => setCountries(res.features));
     }, []);
 
     // resizing window
@@ -28,19 +34,42 @@ const Globe = () => {
 
     console.log(windowWidth, windowHeight)
 
+    // gen earth-like texture function
+
+    const genRandColor = () => {
+        let r = Math.floor(Math.random()*100 + 100).toString(16)
+        let g = Math.floor(Math.random()*100 + 155).toString(16)
+        let b = Math.floor(Math.random()*50 + 40).toString(16)
+
+        let res = '#' + r + g +b
+        return  res
+    }
+
+    // custom globe material
+
+    const globeMaterial = new THREE.MeshPhongMaterial(
+        {
+            color: 0x0489AF,
+            specular: 0x270AFF,
+            shininess: 30,
+        }
+    )
+
     return (
         <div className='flex justify-center my-12 md:my-5'>
             <Earth
                 width={windowWidth}
                 height={windowHeight/2}
-                globeImageUrl="earth_texture.jpg"
-                backgroundColor='#E8EEF2'
+                // globeImageUrl="https:///unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+                // bumpImageUrl="https://unpkg.com/three-globe/example/img/earth-topology.png"
+                globeMaterial={globeMaterial}
+                backgroundColor={'#E8EEF2'}
                 polygonsData={countries}
                 polygonResolution={3}
                 polygonMargin={0.3}
-                polygonCapColor={() => '#00FF00'}
-                polygonStrokeColor={() => `#000000`}
+                polygonCapColor={() => genRandColor()}
                 polygonSideColor={() => '#000000'}
+                polygonStrokeColor={() => false}
             />
         </div>
     );
