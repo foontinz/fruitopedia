7598bac83e86b20134e83721234b4d5ee08901bc
foontinz <foontinz@protonmail.com>
2023-02-16 06:15:53 +0100

    Adds imports for new models: Country, Fruit, Variety, country_variety.

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,3 +1,6 @@
 from app.db.base_class import Base  # noqa
 from app.models.user import User  # noqa
-# from app.models.user import User  # noqa
+from app.models.country import Country  # noqa
+from app.models.fruit import Fruit  # noqa
+from app.models.variety import Variety  # noqa
+from app.models.country_variety import country_variety  # noqa
