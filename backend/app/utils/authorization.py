008b28d71b0b614401a67bfd26347664e7709bd2
foontinz <foontinz@protonmail.com>
2023-02-17 01:48:17 +0100

    Renames salt_hash_password_user to UserCredentials_to_UserCreate. Simplifies logic, adjusts func to new schemas.

--- backend/app/utils/authorization.py
+++ backend/app/utils/authorization.py
@@ -1,8 +1,11 @@
-from app.schemas.user import UserCreateCredentials
+from app.schemas.user import UserCreateCredentials, UserCreate
 from bcrypt import hashpw, gensalt
 
 
-def salt_hash_user_password(user: UserCreateCredentials) -> UserCreateCredentials:
-    user.salt = gensalt().decode('utf-8')
-    user.hashed_password = hashpw(password=user.password.encode('utf-8'), salt=user.salt.encode('utf-8')).decode('utf-8')
-    return user
+def UserCredentials_to_UserCreate(user: UserCreateCredentials) -> UserCreate:
+    salt = gensalt().decode('utf-8')
+    return UserCreate(
+        email=user.email,
+        username=user.username,
+        hashed_password=hashpw(password=user.password.encode('utf-8'),salt=salt.encode('utf-8')).decode('utf-8'),
+        salt=salt)
