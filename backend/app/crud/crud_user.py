5f59cbe2c05e1e473fb503cc39db018067f5e927
foontinz <foontinz@protonmail.com>
2023-02-17 01:41:16 +0100

    Adds TypeVars for email&username, simplify logic CRUDUser of methods. Deletes read_by_identifier.

--- backend/app/crud/crud_user.py
+++ backend/app/crud/crud_user.py
@@ -1,19 +1,23 @@
 from sqlalchemy.orm import Session
-from fastapi.security import OAuth2PasswordRequestForm
 
 from app.models.user import User
 from app.crud.base import CRUDBase
 from app.schemas.user import UserCreate, UserRead, UserMultiRead, UserUpdate, UserDelete, UserLoginCredentials
 from app.core.security import verify_password
 
+from typing import TypeVar
+
+usernameStr = TypeVar('usernameStr', bound=str)
+emailStr = TypeVar('emailStr', bound=str)
+
 class CRUDUser(CRUDBase[User, UserCreate, UserRead, UserMultiRead, UserUpdate, UserDelete]):
     
     
     def create(self, db: Session, *, obj_in: UserCreate) -> User:
-        return super().create(db, obj_in=UserCreate(**obj_in.dict(exclude_none=True, exclude={'password'})))
+        return super().create(db, obj_in=obj_in)
     
     def read(self, db: Session, *, obj_in: UserRead) -> User:
-        return super().read(db, obj_in=UserRead(**obj_in.dict(exclude_none=True)))
+        return super().read(db, obj_in=obj_in)
     
     def read_multi(self, db: Session, *, obj_in: UserMultiRead) -> list[User]:
         return super().read_multi(db, obj_in=obj_in)
@@ -24,17 +28,14 @@ class CRUDUser(CRUDBase[User, UserCreate, UserRead, UserMultiRead, UserUpdate, U
     def delete(self, db: Session, *, obj_in: UserDelete) -> User:
         return super().delete(db, obj_in=obj_in)
     
-    def read_by_email(self, db: Session, *, obj_in: UserRead) -> User:
-        return self.read(db, obj_in=UserRead(email=obj_in.email))
+    def read_by_email(self, db: Session, *, email: emailStr) -> User:
+        return self.read(db, obj_in=UserRead(email=email))
     
-    def read_by_username(self, db: Session, *, obj_in: UserRead) -> User:
-        return self.read(db, obj_in=UserRead(username=obj_in.username))
+    def read_by_username(self, db: Session, *, username: usernameStr) -> User:
+        return self.read(db, obj_in=UserRead(username=username))
     
-    def read_by_identifier(self, db: Session, *, obj_in: UserRead) -> User:
-        return self.read_or(db, obj_in=UserRead(id=obj_in.id, email=obj_in.email, username=obj_in.username))
-
     def authenticate(self, db: Session, *, obj_in: UserLoginCredentials) -> User:
-        user = self.read_by_identifier(db, obj_in=UserRead(**obj_in.dict(exclude_none=True)))
+        user = self.read(db, obj_in=UserRead(**obj_in.dict()))
         if not user or not verify_password(obj_in.password, user.hashed_password, user.salt):
             return None
         return user
