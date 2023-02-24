1a47045786f0a043f8ea861803c996e5faf84d3e
dmitriy <dima22022005@gmail.com>
2023-02-24 01:43:03 +0100

    Make Hero component, pass it to the homepage.

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className='text-[#37393A] mt-2 text-center font-light'>
+          <h1 className='text-[38px] md:text-[50px]'>
+            Fruitopedia
+          </h1>
+          <h1 className='text-[10px] md:text-[10px] md:mt-2'>
+            From people to people
+          </h1>
+        </div>
+    );
+};
+
+export default Hero;
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,23 +2,15 @@ import React from 'react'
 import Searchbar from './Searchbar';
 import Navbar from './Navbar';
 import Globe from './Globe'
+import Hero from './Hero';
 
 const Home = () => {
   return (
     <div>
-      <Navbar></Navbar>
-      <div className='text-[#37393A]'>
-        <div className='mt-2 text-center font-light'>
-          <h1 className='text-[38px] md:text-[50px]'>
-            Fruitopedia
-          </h1>
-          <h1 className='text-[10px] md:text-[10px] md:mt-2'>
-            From people to people
-          </h1>
+          <Navbar></Navbar>
+          <Hero></Hero>
           <Searchbar></Searchbar>
           <Globe></Globe>
-        </div>
-      </div>
     </div>
   );
 };
