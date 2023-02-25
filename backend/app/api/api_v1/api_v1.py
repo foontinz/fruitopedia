c5d0e98bce057a14ab730498241a27423977fbb0
foontinz <foontinz@protonmail.com>
2023-02-26 00:02:01 +0100

    Adds new migration's revision.

--- backend/app/api/api_v1/api_v1.py
+++ backend/app/api/api_v1/api_v1.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
 from app.api.api_v1.endpoints import login
+from app.api.api_v1.endpoints import fruit
 
 api_router = APIRouter()
 api_router.include_router(login.router, prefix="/login", tags=["login"])
-# api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(fruit.router, prefix="/fruit", tags=["fruit"])
 # api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 # api_router.include_router(items.router, prefix="/items", tags=["items"])
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
@@ -7,6 +7,15 @@ from app.api.api_v1.api_v1 import api_router
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
+
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 
 
