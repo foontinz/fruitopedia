06c348a27fa6a300d3eba6a518650403bf3082ad
foontinz <foontinz@protonmail.com>
2023-02-26 02:17:12 +0100

    Adds new endpoints for variety's and fruit's CRUD.

--- backend/app/api/api_v1/api_v1.py
+++ backend/app/api/api_v1/api_v1.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter
 
 from app.api.api_v1.endpoints import login
 from app.api.api_v1.endpoints import fruit
+from app.api.api_v1.endpoints import variety
 
 api_router = APIRouter()
 api_router.include_router(login.router, prefix="/login", tags=["login"])
 api_router.include_router(fruit.router, prefix="/fruit", tags=["fruit"])
-# api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
+api_router.include_router(variety.router, prefix="/variety", tags=["variety"])
 # api_router.include_router(items.router, prefix="/items", tags=["items"])
---
+++ backend/app/api/api_v1/endpoints/variety.py
@@ -0,0 +1,68 @@
+from fastapi import APIRouter, Depends, HTTPException, Body
+from sqlalchemy.orm import Session
+
+from app import crud, schemas
+from app.api.deps import get_db
+from app.utils.convertion_misc import VarietyModel_to_VarietyResponseBody
+
+router = APIRouter()
+
+''' GET /variety/{id}'''
+@router.get("/{id}", response_model=schemas.VarietyResponseBody)
+async def read_variety(
+    id: int,
+    db: Session = Depends(get_db)):
+    
+    variety = crud.variety.read(db, obj_in=schemas.VarietyRead(id=id))
+    if not variety:
+        raise HTTPException(status_code=404, detail="Variety not found")
+    
+    return VarietyModel_to_VarietyResponseBody(variety)
+
+''' PUT /variety/{id}'''
+@router.put("/{id}", response_model=schemas.VarietyResponseBody)
+async def update_variety(
+    id: int,
+    variety_body: schemas.VarietyRequestBody = Body(...),
+    db: Session = Depends(get_db)):
+    
+    variety_create = crud.variety.VarietyRequestBody_to_VarietyCreate(db, variety_body=variety_body)
+    if not variety_create:
+        raise HTTPException(status_code=422, detail="Fruit or countries for variety are not found")
+    
+    variety = crud.variety.update(db, obj_in=schemas.VarietyUpdate(id=id, update_to_obj=variety_create))
+    if not variety:
+        raise HTTPException(status_code=400, detail="Error while updating variety")
+    
+    return VarietyModel_to_VarietyResponseBody(variety)
+
+''' Delete /variety/{id}'''
+@router.delete("/{id}", response_model=schemas.VarietyResponseBody)
+async def delete_variety(
+    id: int,
+    db: Session = Depends(get_db)):
+    
+    variety = crud.variety.delete(db, obj_in=schemas.VarietyDelete(id=id))
+    if not variety:
+        raise HTTPException(status_code=400, detail="Error while deleting variety")
+    
+    return VarietyModel_to_VarietyResponseBody(variety)
+
+''' POST /variety/'''
+@router.post("/", response_model=schemas.VarietyResponseBody)
+async def create_variety(
+    variety_body: schemas.VarietyRequestBody = Body(...),
+    db: Session = Depends(get_db)):
+
+    if crud.variety.read_by_name(db, name=variety_body.name) in crud.variety.read_by_fruit_id(db, fruit_id=variety_body.fruit):
+        raise HTTPException(status_code=400, detail="Variety is already exist")
+    
+    variety_create = crud.variety.VarietyRequestBody_to_VarietyCreate(db, variety_body=variety_body)
+    if not variety_create:
+        raise HTTPException(status_code=422, detail="Fruit or countries for variety are not found")
+    
+    variety = crud.variety.create(db, obj_in=variety_create)
+    if not variety:
+        raise HTTPException(status_code=400, detail="Error while creating variety")
+    
+    return VarietyModel_to_VarietyResponseBody(variety)
