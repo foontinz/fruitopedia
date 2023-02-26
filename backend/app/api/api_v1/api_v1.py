1f2161be9e44fee313fbe8e899d576eee74ed4d2
foontinz <foontinz@protonmail.com>
2023-02-26 15:40:54 +0100

    Adds endpoint /api/v1/country/, adjusts other endpoints

--- backend/app/api/api_v1/api_v1.py
+++ backend/app/api/api_v1/api_v1.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import login
-from app.api.api_v1.endpoints import fruit
-from app.api.api_v1.endpoints import variety
+from app.api.api_v1.endpoints import login, fruit, variety, country
+
 
 api_router = APIRouter()
 api_router.include_router(login.router, prefix="/login", tags=["login"])
 api_router.include_router(fruit.router, prefix="/fruit", tags=["fruit"])
 api_router.include_router(variety.router, prefix="/variety", tags=["variety"])
-# api_router.include_router(items.router, prefix="/items", tags=["items"])
+api_router.include_router(country.router, prefix="/country", tags=["country"])
