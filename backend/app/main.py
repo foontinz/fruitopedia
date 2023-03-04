dcb052f0b6a75c8a8999eb8d29e8360983e040dd
foontinz <foontinz@protonmail.com>
2023-03-04 16:43:49 +0100

    Adjusts main.py and env.py to be compatible with tests environment.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
 from app.core.config import settings
 from app.db.init_db import init_db
 from app.api.api_v1.api_v1 import api_router
-from app.db.session import SessionLocal
-
+from app.api.deps import get_db
 
 app = FastAPI(title=settings.PROJECT_NAME)
+app.include_router(api_router, prefix=settings.API_V1_STR)
+
 
 # Initialize db
-init_db(SessionLocal())
+if __name__ == "__main__":
+    init_db((get_db()))   
 
 
-app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
