c6eeb25415e24e72f7925631e2510613ce3da981
foontinz <foontinz@protonmail.com>
2023-03-26 11:41:28 +0200

    Removes overriding of generic methods, add read_by_variety_id and read_by_by_country_id.

--- backend/app/crud/crud_fruit.py
+++ backend/app/crud/crud_fruit.py
@@ -1,55 +1,45 @@
 from sqlalchemy.orm import Session
-from typing import TypeVar
 
 from app.models import Fruit, Variety, Country
+
 from app.crud.base import CRUDBase
-from app.schemas.fruit import FruitCreate, FruitUpdate, FruitRead, FruitMultiRead, FruitDelete, FruitRequest, FruitMultiReadByCountry, FruitResponse
+from app.crud.types import fruitName, countryId, varietyId
+from app.schemas.fruit import (
+    FruitCreate, FruitUpdate, FruitDelete, 
+    FruitRead, FruitReadMulti, FruitReadAll,
+    FruitRequest, FruitResponse, FruitMultiResponse, 
+    FruitReadByVarietyQueryParams, FruitReadByCountryQueryParams 
+)
+from app.schemas.fruit import Fruit as FruitSchema
 
-fruitName = TypeVar('fruitName', bound=str)
-
-class CRUDFruit(CRUDBase[Fruit, FruitCreate, FruitRead, FruitMultiRead, FruitUpdate, FruitDelete]):
+class CRUDFruit(CRUDBase[Fruit, FruitSchema, FruitCreate, 
+                         FruitRead, FruitReadMulti, FruitReadAll, 
+                         FruitUpdate, FruitDelete, 
+                         FruitResponse, FruitMultiResponse]):
     
-    def create(self, db: Session, *, obj_in: FruitCreate) -> Fruit | None:
-        return super().create(db, obj_in=obj_in)
-        
-    def read(self, db: Session, *, obj_in: FruitRead) -> Fruit | None:
-        return super().read(db, obj_in=obj_in)
     
-    def read_multi(self, db: Session, *, obj_in: FruitMultiRead) -> list[Fruit]:
-        return super().read_multi(db, obj_in=obj_in)
-    
-    def read_multi_by_country_id(self, db: Session, *, obj_in: FruitMultiReadByCountry) -> list[Fruit]:
-        country = db.query(Country).filter(Country.id == obj_in.country_id).first()
-        fruits = set([variety.fruit for variety in country.own_varieties][obj_in.skip:obj_in.limit])
+    def read_by_country_id(self, db: Session, *, country_id: countryId, obj_in: FruitReadByCountryQueryParams) -> list[Fruit]:
+        country = db.query(Country).filter(Country.id == country_id).first()
+        fruits = set([variety.fruit for variety in country.own_varieties][obj_in.skip:obj_in.limit + obj_in.skip])
         return list(fruits)
     
-    def update(self, db: Session, *, obj_in: FruitUpdate) -> Fruit | None:
-        return super().update(db, obj_in=obj_in)        
-    
-    def delete(self, db: Session, *, obj_in: FruitDelete) -> Fruit | None:
-        return super().delete(db, obj_in=obj_in)
+    def read_by_variety_id(self, db: Session, *, variety_id: varietyId) -> Fruit | None:
+        variety = db.query(Variety).filter(Variety.id == variety_id).first()
+        return variety.fruit
     
     def read_by_name(self, db: Session, *, name: fruitName) -> Fruit | None:
         return db.query(Fruit).filter(self.model.name == name).first()
-    
 
     def request_to_create(self, db: Session, *, fruit_body: FruitRequest) -> FruitCreate | None:
-        varieties = [db.query(Variety).filter(Variety.id == variety_id).first() for variety_id in fruit_body.varieties]
+        varieties = db.query(Variety).filter(Variety.id.in_(fruit_body.varieties)).all()
         
-        return FruitCreate(
+        return FruitCreate.construct(
             name=fruit_body.name,
             description=fruit_body.description,
             varieties=varieties
         )
 
-    def model_to_response_body(self, db: Session, *, fruit: Fruit, detailed: bool = False) -> FruitResponse:
-        fruit_response = FruitResponse(
-            id=fruit.id,
-            name=fruit.name
-        )
-        if detailed:
-            fruit_response.description = fruit.description
-            fruit_response.varieties = [variety.id for variety in fruit.varieties]
-        return fruit_response
-
+        
+       
+        
 fruit = CRUDFruit(Fruit)
