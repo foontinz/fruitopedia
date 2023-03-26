8f7126aaeea9c553584da4e70d1de9cc3817f20f
foontinz <foontinz@protonmail.com>
2023-03-26 11:40:33 +0200

    Adds file types.py for custom types.

---
+++ backend/app/crud/types.py
@@ -0,0 +1,17 @@
+from typing import NewType
+
+
+fruitName = NewType('fruitName', str)
+fruitId = NewType('fruitId', int)
+
+countryName = NewType('countryName', str)
+countryId = NewType('countryId', int)
+countryISOCode = NewType('countryISOCode', str)
+
+varietyName = NewType('varietyName', str)
+varietyId = NewType('varietyId', int)
+
+
+usernameStr = NewType('usernameStr', str)
+emailStr = NewType('emailStr', str)
+
