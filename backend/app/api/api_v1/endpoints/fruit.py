132d16f1b597b92b1be548b1b4eb50f4885cf6df
foontinz <foontinz@protonmail.com>
2023-03-26 11:34:43 +0200

    Adds new endpoints for fruit. Changes params to include fields. Adjusts names of renamed methods.

--- backend/app/api/api_v1/endpoints/fruit.py
+++ backend/app/api/api_v1/endpoints/fruit.py
@@ -7,6 +7,8 @@ from app.api.deps import get_db
 from app.api.responses import RESPONSES
 from app.crud.validators.error import ValidationError
 from app.crud.validators.fruit_validators import FruitValidators
+from app.crud.validators.country_validators import CountryValidators
+from app.crud.validators.variety_validators import VarietyValidators
 
 router = APIRouter()
 
@@ -14,29 +16,76 @@ router = APIRouter()
 @router.get("/country/{id}", responses=RESPONSES["MULTI_GET"], status_code=status.HTTP_200_OK)
 async def read_fruits_by_country(
     id: int,
-    params: schemas.MultiReadQueryParams = Depends(),
+    params: schemas.FruitReadByCountryQueryParams = Depends(),
     db: Session = Depends(get_db)):
+
+    try:
+        CountryValidators.validate_read(db, country_id=id, params=params)
+    except ValidationError as e:
+        return JSONResponse(status_code=e.code, content={"message": e.message})
     
-    fruits = crud.fruit.read_multi_by_country_id(db, obj_in=schemas.FruitMultiReadByCountry(country_id=id, **params.dict()))
-    fruits_dict = schemas.FruitMultiResponse(
-        fruits=[crud.fruit.model_to_response_body(db, fruit=fruit, detailed=params.detailed) for fruit in fruits]
-        ).dict(exclude_unset=True)
+    fruits = crud.fruit.read_by_country_id(
+        db, obj_in=schemas.FruitMultiReadByCountryQueryParams(country_id=id, **params.dict()))
+    fruits_dict = crud.fruit.model_to_multi_response_body(
+        objs=fruits, params=params).dict(exclude_unset=True)
+
     return JSONResponse(status_code=status.HTTP_200_OK, content=fruits_dict)
 
-
-''' GET /fruit/{id}'''
-@router.get("/{id}", responses=RESPONSES["GET"], response_model_exclude_unset=True)
-async def read_fruit(
+''' GET /fruit/variety/{id}'''
+@router.get("/variety/{id}", responses=RESPONSES["GET"], status_code=status.HTTP_200_OK)
+async def read_fruit_by_variety(
     id: int,
-    detailed: bool = False,
+    params: schemas.FruitReadByVarietyQueryParams = Depends(),
     db: Session = Depends(get_db)):
     
-    fruit = crud.fruit.read(db, obj_in=schemas.FruitRead(id=id))
-    if not fruit:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"message": "Fruit not found"})
+    try:
+        VarietyValidators.validate_read(db, variety_id=id, params=params)
+    except ValidationError as e:
+        return JSONResponse(status_code=e.code, content={"message": e.message})
     
-    fruit_dict = crud.fruit.model_to_response_body(db, fruit=fruit, detailed=detailed).dict(exclude_unset=True)
+    fruits = crud.fruit.read_by_variety_id(db, obj_in=schemas.FruitMultiReadByVariety(variety_id=id, **params.dict()))
+    fruits_dict = crud.fruit.model_to_multi_response_body(
+        objs=fruits, params=params).dict(exclude_unset=True)
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content=fruits_dict)
+
+''' GET /fruit/{id}'''
+@router.get("/{id}", responses=RESPONSES["GET"], status_code=status.HTTP_200_OK)
+async def read_fruit(
+    id: int,
+    params: schemas.FruitReadQueryParams = Depends(),
+    db: Session = Depends(get_db)):
+    
+
+    try:
+        FruitValidators.validate_read(db, fruit_id=id, params=params)
+    except ValidationError as e:
+        return JSONResponse(status_code=e.code, content={"message": e.message})
+    
+    fruit = crud.fruit.read(db, obj_in=schemas.FruitRead(id=id))
+    fruit_dict = crud.fruit.model_to_response_body(
+        obj=fruit, params=params).dict(exclude_unset=True)
+    
     return JSONResponse(status_code=status.HTTP_200_OK, content=fruit_dict)
+
+''' GET /fruit/'''
+@router.get("/", responses=RESPONSES["MULTI_GET"], status_code=status.HTTP_200_OK)
+async def read_fruits(
+    params: schemas.FruitReadMultiQueryParams = Depends(),
+    db: Session = Depends(get_db)):
+    
+
+    try:
+        FruitValidators.validate_read_multi(db, params=params)
+    except ValidationError as e:
+        return JSONResponse(status_code=e.code, content={"message": e.message})
+
+    fruits = crud.fruit.read_multi(db, obj_in=params) if params.ids else crud.fruit.read_all(db, obj_in=params)
+    fruits_dict = crud.fruit.model_to_multi_response_body(
+        objs=fruits, params=params).dict(exclude_unset=True)
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content=fruits_dict)
+
 
 ''' PUT /fruit/{id}'''
 @router.put("/{id}", responses=RESPONSES["PUT"], status_code=status.HTTP_200_OK)
@@ -46,17 +95,18 @@ async def update_fruit(
     db: Session = Depends(get_db)):
     
     try:
-        FruitValidators.validate_update(db, id=id, fruit_request=fruit_body)
-    except ValidationError as e:
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message": str(e)})
+        FruitValidators.validate_update(db, fruit_id=id, fruit_request=fruit_body)
+    except ValidationError as e:    
+        return JSONResponse(status_code=e.code, content={"message": e.message})
     
     fruit_create = crud.fruit.request_to_create(db, fruit_body=fruit_body)
-
     fruit = crud.fruit.update(db, obj_in=schemas.FruitUpdate(id=id, update_to_obj=fruit_create))
     if not fruit:
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"message":"Error while updating fruit"})
     
-    fruit_dict = crud.fruit.model_to_response_body(db, fruit=fruit, detailed=True).dict(exclude_unset=True)
+    fruit_dict = crud.fruit.model_to_response_body(
+        obj=fruit, params=schemas.FruitReadQueryParams()).dict(exclude_unset=True)
+    
     return JSONResponse(status_code=status.HTTP_200_OK, content=fruit_dict)
 
 ''' DELETE /fruit/{id}'''
@@ -66,28 +116,15 @@ async def delete_fruit(
     db: Session = Depends(get_db)):
 
     try:
-        FruitValidators.validate_delete(db, id=id)
+        FruitValidators.validate_delete(db, fruit_id=id)
     except ValidationError as e:
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message": str(e)})
-
+        return JSONResponse(status_code=e.code, content={"message": e.message})
+    
     if crud.fruit.delete(db, obj_in=schemas.FruitDelete(id=id)):
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"message": "Error while deleting fruit"})
     
-''' GET /fruit/'''
-@router.get("/", responses=RESPONSES["GET"], status_code=status.HTTP_200_OK)
-async def read_fruits(
-    params: schemas.MultiReadQueryParams = Depends(),
-    db: Session = Depends(get_db)):
-    
-    fruits = crud.fruit.read_multi(db, obj_in=schemas.FruitMultiRead(query_params=params))
-    fruits_dict = schemas.FruitMultiResponse(
-        fruits=[crud.fruit.model_to_response_body(db, fruit=fruit, detailed=params.detailed) for fruit in fruits]
-        ).dict(exclude_unset=True)
-    
-    return JSONResponse(status_code=status.HTTP_200_OK, content=fruits_dict)
-
 ''' POST /fruit/'''
-@router.post("/", responses=RESPONSES["POST"], response_model_exclude_unset=True, status_code=status.HTTP_201_CREATED)
+@router.post("/", responses=RESPONSES["POST"], status_code=status.HTTP_201_CREATED)
 async def create_fruit(
     fruit_body: schemas.FruitRequest = Body(...),
     db: Session = Depends(get_db)):
@@ -95,12 +132,14 @@ async def create_fruit(
     try:
         FruitValidators.validate_create(db, fruit_request=fruit_body)
     except ValidationError as e:
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message": str(e)})
-
+        return JSONResponse(status_code=e.code, content={"message": e.message})
+    
     fruit_create = crud.fruit.request_to_create(db, fruit_body=fruit_body)    
     fruit = crud.fruit.create(db, obj_in=fruit_create)
     if not fruit:
-        raise JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error while creating fruit")
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error while creating fruit")
     
-    fruit_dict = crud.fruit.model_to_response_body(db, fruit=fruit, detailed=True).dict(exclude_unset=True)
+    fruit_dict = crud.fruit.model_to_response_body(
+        obj=fruit, params=schemas.FruitReadQueryParams()).dict(exclude_unset=True)
+    
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=fruit_dict)
