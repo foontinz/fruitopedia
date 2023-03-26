1b87648b608a978dcd807ff3cbdc0914c1e305a0
foontinz <foontinz@protonmail.com>
2023-03-26 11:38:14 +0200

    Make FruitValidators to inherit ValidatorsBase. Adjusts code responses for errors.

--- backend/app/crud/validators/fruit_validators.py
+++ backend/app/crud/validators/fruit_validators.py
@@ -1,14 +1,18 @@
 from app.crud import country, fruit, variety
 from app.crud.crud_variety import varietyName
 from app.crud.validators.error import ValidationError
-from app.schemas.fruit import FruitCreate, FruitRead, FruitMultiRead, FruitUpdate, FruitDelete, FruitRequest, FruitResponse
-from app.schemas.variety import VarietyCreate, VarietyRead, VarietyMultiRead, VarietyUpdate, VarietyDelete, VarietyRequest, VarietyResponse
+from app.crud.validators.base import ValidatorsBase
+from app.schemas.fruit import FruitRead, FruitRequest
+from app.schemas.variety import VarietyRead
 from app.models import Fruit, Variety
 
 from sqlalchemy.orm import Session
-
-class FruitValidators:
+from fastapi import status
+class FruitValidators(ValidatorsBase):
     
+    ALLOWED_FIELDS = ['id', 'name', 'description', 'varieties']
+    MODEL = Fruit
+
     @staticmethod
     def _is_fruit_exist(db: Session, fruit_id: int) -> Fruit | None:
         return fruit.read(db, obj_in=FruitRead(id=fruit_id))
@@ -23,16 +27,17 @@ class FruitValidators:
     
     @classmethod
     def validate_update(cls, db: Session, fruit_id:int, fruit_request: FruitRequest) -> None:
+        
         if not cls._is_fruit_exist(db=db, fruit_id=fruit_id):
-            raise ValidationError(message='Fruit does not exist')
+            raise ValidationError(code=status.HTTP_404_NOT_FOUND, message='Fruit does not exist')
         
         if (fruit := cls._is_fruit_name_exist(
             db=db, name=fruit_request.name)) and fruit.id != fruit_id:
-                raise ValidationError(message='Fruit with this name already exists')
+                raise ValidationError(code=status.HTTP_400_BAD_REQUEST, message='Fruit with this name already exists')
         
         if not all(cls._are_varieties_exist(
             db=db, variety_ids=fruit_request.own_varieties)):
-            raise ValidationError(message='Not all varieties exist')
+            raise ValidationError(code=status.HTTP_400_BAD_REQUEST, message='Not all varieties exist')
         
         return 
     
@@ -40,17 +45,17 @@ class FruitValidators:
     def validate_create(cls, db: Session, fruit_request: FruitRequest) -> None:
         if cls._is_name_exist(
             db=db, name=fruit_request.name):
-            raise ValidationError(message='Fruit with this name already exists')
+            raise ValidationError(code=status.HTTP_400_BAD_REQUEST, message='Fruit with this name already exists')
         
         if not all(cls._are_varieties_exist(
             db=db, variety_ids=fruit_request.varieties)):
-            raise ValidationError(message="Not all fruit's varieties exist")
+            raise ValidationError(code=status.HTTP_400_BAD_REQUEST, message="Not all fruit's varieties exist")
         
         return 
     
     @classmethod
     def validate_delete(cls, db: Session, fruit_id: int) -> None:
         if not cls._is_fruit_exist(db=db, fruit_id=fruit_id):
-            raise ValidationError(message='Fruit does not exist')
+            raise ValidationError(code=status.HTTP_404_NOT_FOUND, message='Fruit does not exist')
         
         return
