bd7cf59a1e284c874b6f4cdede176cd20ecbb0f8
foontinz <foontinz@protonmail.com>
2023-03-26 11:34:24 +0200

    Adds new endpoints for country. Changes params to include fields. Adjusts names of renamed methods.

--- backend/app/api/api_v1/endpoints/country.py
+++ backend/app/api/api_v1/endpoints/country.py
@@ -7,6 +7,9 @@ from app.api.deps import get_db
 from app.api.responses import RESPONSES
 from app.crud.validators.error import ValidationError
 from app.crud.validators.country_validators import CountryValidators
+from app.crud.validators.variety_validators import VarietyValidators
+from app.crud.validators.fruit_validators import FruitValidators
+
 router = APIRouter()
 
 ''' GET /country/fruit/{id}'''
@@ -14,29 +17,72 @@ router = APIRouter()
 async def read_countries_by_fruit_id(
     id: int,
     db: Session = Depends(get_db),
-    params: schemas.MultiReadQueryParams = Depends()):
+    params: schemas.ReadAllQueryParams = Depends()):
 
-    countries = crud.country.read_multi_by_fruit_id(db, obj_in=schemas.CountryMultiReadByFruit(fruit_id=id, **params.dict()))
-    
-    countries_dict = schemas.CountryMultiResponse(
-        countries=[crud.country.model_to_response_body(db, country=country, detailed=params.detailed) for country in countries]
-        ).dict(exclude_unset=True)
+    try:
+        FruitValidators.validate_read(db, fruit_id=id, params=params)
+    except ValidationError as e:
+        return JSONResponse(status_code=e.code, content={"message": e.message})
+
+    countries = crud.country.read_by_fruit_id(db, id=id, obj_in=params)
+    countries_dict = crud.country.model_to_multi_response_body(
+        objs=countries, params=params).dict(exclude_unset=True)
     
     return JSONResponse(status_code=status.HTTP_200_OK, content=countries_dict)
+
+''' GET /country/variety/{id}'''
+@router.get("/variety/{id}", responses=RESPONSES["MULTI_GET"], status_code=status.HTTP_200_OK)
+async def read_countries_by_variety_id(
+    id: int,
+    db: Session = Depends(get_db),
+    params: schemas.CountryReadByVarietyQueryParams = Depends()):
+
+    try: 
+        VarietyValidators.validate_read(db, country_id=id, params=params)
+    except ValidationError as e:
+        return JSONResponse(status_code=e.code, content={"message": e.message})
+    
+    countries = crud.country.read_by_variety_id(db, id=id, obj_in=params)
+    countries_dict = crud.country.model_to_multi_response_body(
+        objs=countries, params=params).dict(exclude_unset=True)
+    
+    return JSONResponse(status_code=status.HTTP_200_OK, content=countries_dict)
+
 
 ''' GET /country/{id}'''
 @router.get("/{id}", responses=RESPONSES["GET"], status_code=status.HTTP_200_OK)
 async def read_country(
     id: int,
-    detailed: bool = False,
-    db: Session = Depends(get_db)):
+    db: Session = Depends(get_db),
+    params: schemas.CountryReadQueryParams = Depends()):
     
-    country = crud.country.read(db, obj_in=schemas.CountryRead(id=id))
-    if not country:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"message": "Country not found"})
+    try:
+        CountryValidators.validate_read(db, country_id=id, params=params)
+    except ValidationError as e:
+        return JSONResponse(status_code=e.code, content={"message": e.message})
+
+    country = crud.country.read(db, obj_in=schemas.CountryRead(id=id))    
+    country_dict = crud.country.model_to_response_body(
+        obj=country, params=params ).dict(exclude_unset=True)
     
-    country_dict = crud.country.model_to_response_body(db, country=country, detailed=detailed).dict(exclude_unset=True)
     return JSONResponse(status_code=status.HTTP_200_OK, content=country_dict)
+
+''' GET /country/'''
+@router.get("/", responses=RESPONSES["MULTI_GET"], status_code=status.HTTP_200_OK)
+async def read_countries(
+    db: Session = Depends(get_db),
+    params: schemas.CountryReadMultiQueryParams = Depends()):
+    
+    try:
+        CountryValidators.validate_read_multi(params=params)
+    except ValidationError as e:
+        return JSONResponse(status_code=e.code, content={"message": e.message})
+
+    countries = crud.country.read_multi(db, obj_in=params) if params.ids else crud.country.read_all(db, obj_in=params)    
+    countries_dict = crud.country.model_to_multi_response_body(
+        objs=countries, params=params).dict(exclude_unset=True)
+    
+    return JSONResponse(status_code=status.HTTP_200_OK, content=countries_dict)
 
 ''' PUT /country/{id}'''
 @router.put("/{id}", responses=RESPONSES["PUT"], status_code=status.HTTP_200_OK)
@@ -48,13 +94,16 @@ async def update_country(
     try:
         CountryValidators.validate_update(db, country_id=id,country_request=country_body)
     except ValidationError as e:
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message":str(e)})
+        return JSONResponse(status_code=e.code, content={"message": e.message})
     
     country_create = crud.country.request_to_create(db, country_body=country_body)
     country = crud.country.update(db, obj_in=schemas.CountryUpdate(id=id, update_to_obj=country_create))
     if not country:
-        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"message":"Error while updating country"})
-    country_dict = crud.country.model_to_response_body(db, country=country, detailed=True).dict(exclude_unset=True)
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"message":"Error while updating country"})
+    
+    country_dict = crud.country.model_to_response_body(
+        obj=country, params=schemas.CountryReadQueryParams()).dict(exclude_unset=True)
 
     return JSONResponse(status_code=status.HTTP_200_OK, content=country_dict)
 
@@ -67,23 +116,14 @@ async def delete_country(
     try: 
         CountryValidators.validate_delete(db, country_id=id)
     except ValidationError as e: 
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message":str(e)})
-
+        return JSONResponse(status_code=e.code, content={"message": e.message})
+    
 
     if crud.country.delete(db, obj_in=schemas.CountryDelete(id=id)):
-        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"message": "Error while deleting country"})
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"message": "Error while deleting country"})
     
-''' GET /country/'''
-@router.get("/", responses=RESPONSES["GET"], status_code=status.HTTP_200_OK)
-async def read_countries(
-    db: Session = Depends(get_db),
-    params: schemas.MultiReadQueryParams = Depends()):
-    
-    countries = crud.country.read_multi(db, obj_in=schemas.CountryMultiRead(skip=params.skip, limit=params.limit, detailed=params.detailed))    
-    countries_dict = schemas.CountryMultiResponse(
-        countries=[crud.country.model_to_response_body(db, country=country, detailed=params.detailed) for country in countries]
-        ).dict(exclude_unset=True)
-    return JSONResponse(status_code=status.HTTP_200_OK, content=countries_dict)
+
 
 ''' POST /country/'''
 @router.post("/", responses=RESPONSES["POST"], status_code=status.HTTP_201_CREATED)
@@ -94,13 +134,16 @@ async def create_country(
     try:
         CountryValidators.validate_create(db, country_request=country_body)
     except ValidationError as e:
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message":str(e)})
+        return JSONResponse(status_code=e.code, content={"message": e.message})
     
     country_create = crud.country.request_to_create(db, country_body=country_body)
     country = crud.country.create(db, obj_in=country_create)
     
     if not country:
-        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"message": "Error while creating country"})
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"message": "Error while creating country"})
     
-    country_dict = crud.country.model_to_response_body(db, country=country, detailed=True).dict(exclude_unset=True)
+    country_dict = crud.country.model_to_response_body(
+        obj=country, params=schemas.CountryReadQueryParams()).dict(exclude_unset=True)
+
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=country_dict)
